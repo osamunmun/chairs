46b7c8e167b1344c98efed4954d51be779f04497
Yasuhiro Asaka <a12z1ya@aiit.ac.jp>
2013-01-26 18:46:21 +0900

    Initial Commit :D
    - Rails 3.2.11
    - Twitter Bootstrap (bootstrap-sass)
    - haml, sass

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'foreman'
+gem 'thin'
+gem 'rails',       '3.2.11'
+gem 'bcrypt-ruby', '~> 3.0.0'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'bootstrap-sass'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'jquery-rails'
+  gem 'uglifier',     '>= 1.0.3'
+end
+
+group :development do
+  gem 'haml-rails'
+  gem 'sqlite3'
+  gem 'wirble'
+  gem 'pry'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chairs::Application.config.secret_token = '79c131ff900e2decff9894b5e2da5874c7cf3b5c973b05f7e378233f167aa698591ba342d4f5b4a54307690b5b9ec3e2b0e7ec6c563df9b6a88de8b5ec968d83'
