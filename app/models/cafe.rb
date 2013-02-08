7327847cc167b7962667cca415325f584294bc64
Yasuhiro Asaka <a12z1ya@aiit.ac.jp>
2013-02-09 05:59:55 +0900

    Update listview ;)
    - Tranparent Icons
    - jQuery mobile init
    - list style

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -2,4 +2,8 @@ class Cafe < ActiveRecord::Base
   attr_accessible :venue_id, :full, :herenow, :image, :location, :name
   alias_attribute :vid, :venue_id
   alias_attribute :now, :herenow
+
+  def full?
+    full
+  end
 end
